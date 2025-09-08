d8560cf55e693cb8217d190742d1eb8813f36ed8
cesarg210 <cesarg210@gmail.com>
2025-09-08 19:01:28 +0100

    Added Task and WorkTask

---
+++ src/Task.java
@@ -0,0 +1,41 @@
+abstract class Task {
+    String title;
+    String description;
+    String status;
+    Category category;
+
+    abstract String getDetails();
+    abstract void updateStatus();
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
---
+++ src/WorkTask.java
@@ -0,0 +1,18 @@
+public class WorkTask extends Task{
+    Assigne assigne;
+    int priority;
+
+    WorkTask(String title, Assigne assigne, String description, String category){
+
+    }
+
+    @Override
+    public String getDetails(){
+
+    }
+
+    @Override
+    public void updateStatus(){
+
+    }
+}
