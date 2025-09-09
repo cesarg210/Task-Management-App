7cef04d3d25b65de7742d22f68c66a11e034c147
cesarg210 <cesarg210@gmail.com>
2025-09-09 21:25:17 +0100

    some edits

--- src/com/taskmanagementapp/Main.java
+++ src/com/taskmanagementapp/Main.java
@@ -1,4 +1,7 @@
 package com.taskmanagementapp;
+import com.taskmanagementapp.models.*;
+import com.taskmanagementapp.datastructures.DoublyLinkedList;
+import com.taskmanagementapp.services.TaskManager;
 
 public class Main {
     public static void main(String[] args){
--- src/com/taskmanagementapp/datastructures/DoublyLinkedList.java
+++ src/com/taskmanagementapp/datastructures/DoublyLinkedList.java
@@ -46,6 +46,7 @@ public class DoublyLinkedList<T> {
             newNode.next = head;
             head = newNode;
         }
+        length++;
     }
 
 
